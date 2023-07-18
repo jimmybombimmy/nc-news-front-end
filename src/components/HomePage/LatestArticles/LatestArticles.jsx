59d7c5f263954f84f57c3e6aa6b05befc5092072
jimmybombimmy <jamespeters92@gmail.com>
2023-07-18 14:24:06 +0100

    import clean-up

--- src/components/HomePage/LatestArticles/LatestArticles.jsx
+++ src/components/HomePage/LatestArticles/LatestArticles.jsx
@@ -1,26 +1,22 @@
-import axios from "axios";
 import React from "react";
-import { useState, useEffect } from "react";
 import ArticlePreview from "./ArticlePreview";
 
-const LatestArticles = ({allArticles, isLoading}) => {
-
+const LatestArticles = ({ allArticles, isLoading }) => {
   return (
     <section id="articlesListSection">
-
-        <section>
-          <h3 id="selectArticlesView">Latest</h3>
-          <h2 id="latestArticlesHeader">Latest Articles</h2>
-          {isLoading === true ? (
-        <h2>Loading Articles...</h2>
-      ) : (
+      <section>
+        <h3 id="selectArticlesView">Latest</h3>
+        <h2 id="latestArticlesHeader">Latest Articles</h2>
+        {isLoading === true ? (
+          <h2>Loading Articles...</h2>
+        ) : (
           <section id="articlesListDisplay">
             {allArticles.map((article) => {
               return <ArticlePreview article={article} key={article.title} />;
             })}
           </section>
-      )}
-        </section>
+        )}
+      </section>
     </section>
   );
 };
