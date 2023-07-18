ebeb73b62032b5f9c55f0bfef9460865c1fc760b
jimmybombimmy <jamespeters92@gmail.com>
2023-07-18 15:13:02 +0100

    amend to allow axios.create

--- src/api/api-calls.jsx
+++ src/api/api-calls.jsx
@@ -1,11 +1,21 @@
 import axios from "axios";
 
+const newsApi = axios.create({
+  baseURL: "https://nc-news-ajy0.onrender.com/api"
+})
+
 const getAllArticles = () => {
-  return axios
-    .get("https://nc-news-ajy0.onrender.com/api/articles")
+  return newsApi.get("/articles")
     .then((response) => {
       return response.data.articles;
     })
 };
+
+const getArticleById = (id) => {
+  return newsApi.get(`/articles/${id}`)
+    .then((response) => {
+      console.log("newresponse", response.data)
+    })
+}
 
 export default getAllArticles
