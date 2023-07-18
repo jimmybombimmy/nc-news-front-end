9506fadc31a88dc90237fafc80bae83f9ed0e969
jimmybombimmy <jamespeters92@gmail.com>
2023-07-18 15:13:26 +0100

    amend date

--- src/components/HomePage/LatestArticles/ArticlePreview.jsx
+++ src/components/HomePage/LatestArticles/ArticlePreview.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const ArticlePreview = ({article}) => {
+  const date = new Date(article.created_at)
+  const dateShort = date.toString().slice(4,15)
+
   return (
     <section className="articlePreview">
       <hr className="hr"></hr>
@@ -9,7 +12,7 @@ const ArticlePreview = ({article}) => {
       <a className="articlePreviewInfo" href="#">Topic: {article.topic}</a>
       <a className="articlePreviewInfo" href="#">Comments: {article.comment_count}</a>
       <a className="articlePreviewInfo" href="#">Votes: {article.votes}</a>
-      <a className="articlePreviewInfo" href="#">Created At: {article.created_at}</a>
+      <a className="articlePreviewInfo" href="#">Created: {dateShort}</a>
       <p></p>
     </section>
   );
